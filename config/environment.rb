c23795721f1e6ec78a261754b67d753e41f7191d
vmatekole <victor@matekole.com>
2017-05-04 23:13:17 +0200

    make mongo url configurable via env

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+if Rails.env.development?
+    ENV['MONGO_URL'] = 'mongo-sb:27017'
+end
